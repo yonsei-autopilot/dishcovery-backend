1f230a3c7326ff602d5204612def9cc1c3735b76
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2025-05-14 10:25:03 +0900

    ♻️ gemini 모델 다시 변경

--- internal/service/menu_translation_service.go
+++ internal/service/menu_translation_service.go
@@ -24,7 +24,7 @@ func TranslateMenu(ctx context.Context, id string, imageBytes []byte, imageForma
 
 	boundingBoxResult, err := gemini.GeminiRequestBuilder().
 		WithContext(ctx).
-		WithModel("gemini-2.5-pro-exp-03-25").
+		WithModel("gemini-2.5-pro-preview-03-25").
 		WithImage(imageBytes, imageFormat).
 		WithTemperature(&temperature).
 		WithTopK(&topk).
